8ce097b3dc9019405a80201574f2868f1a928697
anks321 <kumaralankrit20@gmail.com>
2021-05-21 15:59:17 +0530

    Done

--- numpy.py
+++ numpy.py
@@ -1,1 +1,24 @@
-print("HELLO World")
+import numpy as np
+# initialize arrays
+a= np.array([1,2,3],dtype='int16')
+print(a);
+b=np.array([[1.0,2.0,3.0],[2.0,4.5,6.7]])
+print(b);
+
+
+#getting dimension ans shape
+print(a.ndim)
+print (b.ndim)
+print(a.shape)
+print(b.shape)
+
+
+# Get type
+print(a.dtype)
+
+#get size
+print(a.size)
+#get itemsize
+print(a.itemsize)
+#total size
+print(b.nbytes)
